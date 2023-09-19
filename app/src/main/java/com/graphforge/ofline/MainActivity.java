8b1800da70d479027dc946826a06dbebfa92336e
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-20 01:01:44 +0600

    dependencies added for image crop

--- app/src/main/java/com/graphforge/ofline/MainActivity.java
+++ app/src/main/java/com/graphforge/ofline/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //imagecroper video
+        // https://www.youtube.com/watch?v=DM8vorNKIFg
+
+//githb
+        //https://github.com/krokyze/uCrop-n-Edit
     }
 }
